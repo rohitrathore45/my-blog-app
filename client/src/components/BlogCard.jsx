308c3d1dbe4a73fc8f6b2c00a6f9725428d8679d
Rohit Rathore <rohitjeevanrathore@gmail.com>
2025-07-08 12:33:04 +0530

    complete project

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
+// blog card
+
 const BlogCard = ({blog}) => {
 
     const{title, description, category, image, _id} = blog;
